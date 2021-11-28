e24758263aff848bd70fefe28bea1d95e6aa997c
R k Tiwari <49756216+rktiwari99@users.noreply.github.com>
2021-11-28 22:02:03 +0530

    version 1

---
+++ wp-content/themes/lz-software-company/template-parts/footer/site-info.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @subpackage LZ Software Company
+ * @since 1.0
+ * @version 1.4
+ */
+
+?>
+<div class="site-info">
+	<p><?php echo esc_html(get_theme_mod('lz_software_company_footer_copy',__('Software Company WordPress Theme By Luzuk','lz-software-company'))); ?></p>
+</div>
